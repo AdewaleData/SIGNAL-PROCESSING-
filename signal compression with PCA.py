39860dfc2e4da1532ec80efa5e5d3e681f53ac70
Adewale Afolabi <120673579+AdewaleData@users.noreply.github.com>
2023-03-11 10:52:40 +0100

    Update and rename signal compression with PCA to signal compression with PCA.py

--- signal compression with PCA.py
+++ signal compression with PCA.py
@@ -1,6 +1,6 @@
 
 # DESCRIPTION
-
+'''
 In this example, we first generate a test signal that consists of two sine waves with frequencies of 10 Hz and 20 Hz.
 We then plot the original signal to visualize it.
 
@@ -14,7 +14,7 @@ Finally, we plot the reconstructed signal to visualize the effect of the compres
 
 This is just a simple example, and in practice, there are many ways to choose the number of principal 
 components to keep and how to encode the compressed components for efficient storage or transmission.
-
+'''
 
 
 
